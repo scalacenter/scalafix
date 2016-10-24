e70f42b248462874a125832fe97a4b2fa48e222c
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-10-24 14:54:56 +0200

    Setup caching on drone

--- bin/setupCI.sh
+++ bin/setupCI.sh
@@ -2,5 +2,4 @@
 set -e
 apt-get install nailgun
 alias ng=ng-nailgun
-export PATH=$PATH:/root/local/bin
 pip install --user codecov
--- bin/testAll.sh
+++ bin/testAll.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-sbt clean coverage test
-sbt "; publishLocal ; scripted ; cli/pack"
-sbt coverageAggregate
-
+sbt -Dsbt.home.ivy=/drone/cache/ivy2 clean coverage test
+sbt -Dsbt.home.ivy=/drone/cache/ivy2 "; publishLocal ; scripted ; cli/pack"
+sbt -Dsbt.home.ivy=/drone/cache/ivy2 coverageAggregate
