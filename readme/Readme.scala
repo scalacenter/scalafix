efdbe177af47eebea2239056be2744eace26149c
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-12-09 10:03:23 +0000

    Update docs

--- readme/Readme.scala
+++ readme/Readme.scala
@@ -1,14 +1,16 @@
 package scalafix
 
+import scalatags.Text.TypedTag
 import scalatags.Text.all._
-
 import scalatex._
 import scalatags.Text.all._
 
 object Readme {
+  def note = b("Note. ")
   def github: String = "https://github.com"
   def repo: String = "https://github.com/scalacenter/scalafix"
   def issue(i: Int): Frag = a(href := s"$repo/issues/$i", s"#$i")
   def dotty = a(href := "http://dotty.epfl.ch/", "Dotty")
+  def comment(frags: Frag*): TypedTag[String] = span("")
 
 }
