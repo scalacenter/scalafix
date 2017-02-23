8902012a364efaf89e63b3605f2c7e1a0d64267a
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-02-23 14:37:38 +0100

    Update documentation.
    - Remove old references
    - Add explanations for new stuff
    - Clarify language where needed
    - Add changelog for v0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,6 +11,6 @@ object Dependencies {
   def scalahost(scalaVersion: String): ModuleID = "org.scalameta" % s"scalahost_$scalaVersion" % scalametaV
   def scalameta: ModuleID                       = "org.scalameta" %% "contrib"                 % scalametaV
   def scalatest: ModuleID                       = "org.scalatest" %% "scalatest"               % "3.0.0"
-  def metaconfig: ModuleID                      = "com.geirsson"  %% "metaconfig-hocon"        % "0.1.1"
+  def metaconfig: ModuleID                      = "com.geirsson"  %% "metaconfig-hocon"        % "0.1.2"
   def ammonite                                  = "com.lihaoyi"   %% "ammonite-ops"            % "0.8.2"
 }
