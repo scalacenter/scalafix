bccc6f623147d576c8839ed782b6ea659912bbbf
Matthias Langer <mlangc@gmx.at>
2017-03-12 21:59:47 +0100

    Improved error messages in SBT tests
    This commit makes use of the recent reorganization of the SBT tests by using
    `java-diff-utils` to generate better error messages.

--- scalafix-sbt/src/test-project-template/project/ScalafixTestUtility.scala
+++ scalafix-sbt/src/test-project-template/project/ScalafixTestUtility.scala
@@ -1,9 +1,26 @@
 import sbt.Keys.TaskStreams
+import difflib.DiffUtils
 
 /**
   * Created by ollie on 02/01/17.
   */
 object ScalafixTestUtility {
+  private def calcDiff(expected: String, obtained: String): Option[String] = {
+    import scala.collection.JavaConverters._
+
+    if (expected == obtained) None else {
+      def jLinesOf(s: String) = s.lines.toSeq.asJava
+
+      val expectedLines = jLinesOf(expected)
+      val obtainedLines = jLinesOf(obtained)
+
+      val diff = DiffUtils.diff(expectedLines, obtainedLines)
+      val unifiedDiff = DiffUtils.generateUnifiedDiff("expected", "obtained", expectedLines, diff, 1)
+
+      Some(unifiedDiff.asScala.drop(3).mkString("\n"))
+    }
+  }
+
   // returns true if test succeeded, else false.
   def assertContentMatches(streams: TaskStreams)(
       file: String,
@@ -16,22 +33,23 @@ object ScalafixTestUtility {
         ))
     ).trim
 
-    if (obtained.diff(expected).nonEmpty ||
-        expected.diff(obtained).nonEmpty) {
+    calcDiff(expected, obtained).map { diff =>
       val msg =
         s"""File: $file
            |Obtained output:
+           |----------------
            |$obtained
            |Expected:
+           |---------
            |$expected
            |Diff:
-           |${obtained.diff(expected)}
-           |${expected.diff(obtained)}
-           |""".stripMargin
+           |-----
+           |$diff""".stripMargin
+
       streams.log.error(file)
       streams.log.error(msg)
       false
-    } else {
+    }.getOrElse {
       streams.log.success(file)
       true
     }
