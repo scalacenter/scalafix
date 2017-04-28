4f15340761b181602bf81c63c403b5160128f79e
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-04-28 16:51:25 +0200

    WIP

--- scalafix-core/src/main/scala/scalafix/rewrite/ExplicitUnit.scala
+++ scalafix-core/src/main/scala/scalafix/rewrite/ExplicitUnit.scala
@@ -5,6 +5,7 @@ import scala.meta._
 
 case object ExplicitUnit extends Rewrite {
   override def rewrite(ctx: RewriteCtx): Patch = {
+    org.scalameta.logger.elem(ctx.tree)
     ctx.tree.collect {
       case t: Decl.Def if t.decltpe.tokens.isEmpty =>
         ctx.addRight(t.tokens.last, s": Unit")
