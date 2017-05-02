4259cdd9cdb710803f7a836c8dfc8060eaaa2224
Gabriele Petronella <gabriele@buildo.io>
2017-05-02 16:08:04 +0200

    Add NoAutoTupling semantic rewrite

---
+++ scalafix-core/src/main/scala/scalafix/rewrite/NoAutoTupling.scala
@@ -0,0 +1,51 @@
+package scalafix
+package rewrite
+
+import scala.meta._
+import scala.meta.contrib._
+import scalafix.syntax._
+import scala.meta.semantic.v1.Signature
+
+import org.scalameta.logger
+
+case class NoAutoTupling(mirror: Mirror) extends SemanticRewrite(mirror) {
+
+  private[this] val singleTuplePattern = "^Lscala\\/Tuple\\d{1,2}$".r.pattern
+
+  private[this] def addWrappingParens(ctx: RewriteCtx,
+                                      args: Seq[Term.Arg]): Patch =
+    Seq(
+      ctx.addLeft(args.head.tokens.last, "("),
+      ctx.addRight(args.last.tokens.last, ")")
+    ).asPatch
+
+  override def rewrite(ctx: RewriteCtx): Patch = {
+    ctx.tree.collect {
+      case x @ q"$fun(...$argss)"
+          if argss.length > 0 && fun.isInstanceOf[Ref] =>
+        mirror
+          .symbol(fun.asInstanceOf[Ref])
+          .toOption
+          .collect {
+            case Symbol.Global(_, Signature.Method(_, jvmSignature)) =>
+              jvmSignature.stripPrefix("(").takeWhile(_ != ')').split(';')
+          }
+          .map { argListSignatures =>
+            argListSignatures
+              .zip(argss)
+              .foldLeft(Seq.empty[Patch]) {
+                case (patches, (argListSignature, args)) =>
+                  if (singleTuplePattern
+                        .matcher(argListSignature)
+                        .matches && args.length > 1) {
+                    patches :+ addWrappingParens(ctx, args)
+                  } else {
+                    patches
+                  }
+              }
+              .asPatch
+          }
+          .getOrElse(Patch.empty)
+    }.asPatch
+  }
+}
