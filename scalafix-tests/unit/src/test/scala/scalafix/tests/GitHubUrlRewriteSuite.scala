21c9d1867fded2dc41163afb5c542d23d31550c2
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-06-08 16:40:53 +0200

    Update the github protocol to make it giter8/scalafix-testkit friendly.
    While creating https://github.com/scalacenter/scalafix.g8 I found it
    nice to be able to keep all scalafix related directories together,
    using the following scheme
    
    ```
    scalafix/
      input/    # input tests
      output/   # expected output
      tests/    # scalafix-test tests
      rewrites/ # actual rewrites.
        src/main/scala/fix/ # all rewrites go under the "fix" package
          Repo_v1.scala
    ```
    
    The previous expansion scheme was designed before scalafix-testkit went
    through a major overhaul.

--- scalafix-tests/unit/src/test/scala/scalafix/tests/GitHubUrlRewriteSuite.scala
+++ scalafix-tests/unit/src/test/scala/scalafix/tests/GitHubUrlRewriteSuite.scala
@@ -2,37 +2,42 @@ package scalafix
 package tests
 
 import scalafix.reflect.ScalafixCompilerDecoder.GitHubUrlRewrite
-
-import org.scalatest.{FunSuiteLike, Matchers}
 import metaconfig.Conf
+import org.scalatest.FunSuite
 
-class GitHubUrlRewriteSuite extends FunSuiteLike with Matchers {
-  test("it expands a GitHub shorthand with a default sha") {
-    Conf.Str("github:someorg/somerepo/1.2.3") match {
-      case GitHubUrlRewrite(url) =>
-        url.toString shouldBe "https://github.com/someorg/somerepo/blob/master/scalafix-rewrites/src/main/scala/somerepo/scalafix/Somerepo_1_2_3.scala"
+class GithubUrlRewriteSuite extends FunSuite {
+  def check(original: String, expected: String): Unit = {
+    test(original) {
+      Conf.Str(original) match {
+        case GitHubUrlRewrite(obtained) =>
+          assert(obtained.toString == expected)
+      }
     }
   }
 
-  test("it expands a GitHub shorthand with a specific sha") {
-    Conf.Str("github:someorg/somerepo/1.2.3?sha=master~1") match {
-      case GitHubUrlRewrite(url) =>
-        url.toString shouldBe "https://github.com/someorg/somerepo/blob/master~1/scalafix-rewrites/src/main/scala/somerepo/scalafix/Somerepo_1_2_3.scala"
-    }
-  }
-
-  test("it replaces invalid characters in package name and file name with _") {
-    Conf.Str("github:someorg/some-repo/1.2.3") match {
-      case GitHubUrlRewrite(url) =>
-        url.toString shouldBe "https://github.com/someorg/some-repo/blob/master/scalafix-rewrites/src/main/scala/some_repo/scalafix/Some_repo_1_2_3.scala"
-    }
-  }
-
-  test(
-    "it adds an underscore to the package name and to the file name if the repo name begins with a digit") {
-    Conf.Str("github:someorg/42some-repo/1.2.3") match {
-      case GitHubUrlRewrite(url) =>
-        url.toString shouldBe "https://github.com/someorg/42some-repo/blob/master/scalafix-rewrites/src/main/scala/_42some_repo/scalafix/_42some_repo_1_2_3.scala"
-    }
-  }
+  check(
+    "github:someorg/somerepo/1.2.3",
+    "https://github.com/someorg/somerepo/blob/master/scalafix/rewrites/" +
+      "src/main/scala/fix/Somerepo_1_2_3.scala"
+  )
+  check(
+    "github:someorg/somerepo/1.2.3?sha=master~1",
+    "https://github.com/someorg/somerepo/blob/master~1/scalafix/rewrites/" +
+      "src/main/scala/fix/Somerepo_1_2_3.scala"
+  )
+  check(
+    "github:someorg/some-repo/1.2.3",
+    "https://github.com/someorg/some-repo/blob/master/scalafix/rewrites/" +
+      "src/main/scala/fix/SomeRepo_1_2_3.scala"
+  )
+  check(
+    "github:someorg/42some-repo/1.2.3",
+    "https://github.com/someorg/42some-repo/blob/master/scalafix/rewrites/" +
+      // NOTE: identifiers can't start with numbers like 42. However,
+      // giter8 doesn't support adding a prefix in case the first character
+      // is a number: http://www.foundweekends.org/giter8/Combined+Pages.html#Formatting+template+fields
+      // The rewrite inside the file can still be renamed to _42SomeRepo
+      // without problem.
+      "src/main/scala/fix/42someRepo_1_2_3.scala"
+  )
 }
