6af361cffc29d893a738f8880bd18e3acd55ec28
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-06-08 20:12:05 +0200

    Remove obsolete scripts.

--- bin/before_cache.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-du -h -d 1 $HOME/.ivy2/cache
-du -h -d 2 $HOME/.sbt
-find $HOME/.sbt -name "*.lock" -type f -delete
-find $HOME/.ivy2/cache -name "ivydata-*.properties" -type f -delete
