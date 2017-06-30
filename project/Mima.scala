934c3a68e65ab54109645f8a78e4f1f24bab03ee
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-06-30 17:53:41 +0200

    Document steps for v0.5

--- project/Mima.scala
+++ project/Mima.scala
@@ -1,10 +1,9 @@
-import com.typesafe.tools.mima.core._
-import com.typesafe.tools.mima.core.ProblemFilters._
+jmport com.typesafe.tools.mima.core._
 
 object Mima {
   val ignoredABIProblems: Seq[ProblemFilter] = {
-    Seq(
-      exclude[MissingClassProblem]("scalafix.package$XtensionRewriteCtx")
-    )
+    // After v0.5, start running mima checks in CI and document breaking changes here.
+    // See https://github.com/typesafehub/migration-manager/wiki/sbt-plugin#basic-usage
+    Seq()
   }
 }
