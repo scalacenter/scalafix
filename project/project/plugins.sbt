3dbbb33fd571ed790fcec5611c3aca71605744bf
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-08-15 11:25:49 +0200

    Update 0.5.0 changelog.
    Following feedback from my colleagues to separate the
    "new features" section into two parts: end users and rewrite authors.

--- project/project/plugins.sbt
+++ project/project/plugins.sbt
@@ -1,3 +1,3 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.18")
-addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0-RC9")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.19")
+addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0-RC10")
 unmanagedSources.in(Compile) += baseDirectory.value / ".." / "Dependencies.scala"
--- readme/src/main/scala/scalafix/Readme.scala
+++ readme/src/main/scala/scalafix/Readme.scala
@@ -1,22 +1,28 @@
 package scalafix
 
-import metaconfig._, typesafeconfig._
+import metaconfig._
+import typesafeconfig._
 import scala.meta.inputs.Input
 import scalafix.internal.config.ScalafixMetaconfigReaders
 import scalafix.reflect.ScalafixReflect
 import scalatags.Text.TypedTag
 import scalatags.Text.all._
+import scalatex.Main
 import scalatex.site.Highlighter
 
 object Readme {
+  def users = fa("users")
+  def authors = fa("terminal")
   def sbtkey(name: String, typ: String)(description: Frag*) =
     tr(
       td(code(name)),
       td(code(typ)),
       td(description)
     )
+  def fa(id: String) = i(cls := s"fa fa-$id")
   val highlight = new Highlighter {}
   def note = b("Note. ")
+  def experimental = p(b("Experimental ", fa("warning")))
   def gitter =
     raw(
       """<a href="https://gitter.im/scalacenter/scalafix?utm_source=badge&amp;utm_medium=badge&amp;utm_campaign=pr-badge&amp;utm_content=badge"><img src="https://camo.githubusercontent.com/382ebf95f5b4df9275ac203229928db8c8fd5c50/68747470733a2f2f6261646765732e6769747465722e696d2f6f6c6166757270672f7363616c61666d742e737667" alt="Join the chat at https://gitter.im/scalacenter/scalafix" data-canonical-src="https://badges.gitter.im/scalacenter/scalafix.svg" style="max-width:100%;"></a>""")
