84248d30e103ab6221581dd6e5f2f98d5d2ff811
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-08-30 10:25:30 +0200

    Document SymbolMatcher.

--- scalafix-core/shared/src/main/scala/scalafix/util/SymbolMatcher.scala
+++ scalafix-core/shared/src/main/scala/scalafix/util/SymbolMatcher.scala
@@ -5,11 +5,31 @@ import scala.meta.Symbol
 import scala.meta.Tree
 import scalafix.internal.util.SymbolOps
 
+/**
+  * Utility to match against a particular symbol.
+  *
+  * Can be used both in pattern matching and regular condition testing.
+  * {{{
+  *   val myMethod = SymbolMatcher(Symbol("_root_.myMethod"))
+  *   myMethod.matches(Tree)
+  *   Tree match {
+  *     case myMethod(_) => // act on tree
+  *   }
+  *   myMethod.matches(Tree)
+  * }}}
+  * @param symbols the symbols to match against.
+  * @param sctx the semantic context to lookup symbols of trees.
+  */
 class SymbolMatcher(symbols: List[Symbol])(implicit sctx: SemanticCtx) {
   def matches(tree: Tree): Boolean =
     sctx.symbol(tree).fold(false)(matches)
   def matches(symbol: Symbol): Boolean =
     symbols.exists(x => SymbolOps.isSameNormalized(x, symbol))
+  // Returns Option[Tree] to aid composing multiple unapplies, example:
+  // case myMethod(Name(n)) =>
+  // If it returned a Boolean, then it would not be possible to deconstruct @
+  // bindings, example:
+  // case n @ myMethod() =>  // impossible to deconstruct `n`
   def unapply(tree: Tree): Option[Tree] =
     if (matches(tree)) Some(tree)
     else None
