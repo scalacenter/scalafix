098106d6117abb929f18abbffbdae2d505abc7e6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-08-30 18:30:56 +0200

    Fix readme

--- readme/MyRewrite.scala
+++ readme/MyRewrite.scala
@@ -4,7 +4,7 @@ import scala.meta._
 case object MyRewrite extends Rewrite {
   def rewrite(ctx: RewriteCtx): Patch = {
     ctx.tree.collect {
-      case n: scala.meta.Name => ctx.rename(n, Term.Name(n.syntax + "1"))
+      case n: scala.meta.Name => ctx.replaceTree(n, n.syntax + "1")
     }.asPatch
   }
 }
