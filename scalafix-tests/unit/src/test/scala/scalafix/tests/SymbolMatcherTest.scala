3dcc0849ab800338c4e2eac6c6987a5d3fc4946f
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-08-30 11:42:17 +0200

    Add test for normalized SymbolMatcher.

--- scalafix-tests/unit/src/test/scala/scalafix/tests/SymbolMatcherTest.scala
+++ scalafix-tests/unit/src/test/scala/scalafix/tests/SymbolMatcherTest.scala
@@ -7,15 +7,24 @@ import scalafix.util.SymbolMatcher
 class SymbolMatcherTest extends BaseSemanticTest("SymbolMatcherTest") {
 
   test("matches/unapply") {
-    val explicitReturn =
+    val symbolMatcher =
       SymbolMatcher.exact(Symbol("_root_.test.SymbolMatcherTest."))
     val source = attrs.input.parse[Source].get
     val assertions = source.collect {
-      case explicitReturn(t @ Name(_)) if t.matches(explicitReturn) =>
+      case symbolMatcher(t @ Name(_)) =>
         assert(t.is[Term.Name])
         assert(t.parent.get.is[Defn.Object])
+        assert(symbolMatcher.matches(t))
+        assert(t.matches(symbolMatcher))
     }
-    assert(assertions.nonEmpty)
+    assert(assertions.length == 1)
+  }
+
+  test("normalized") {
+    val term = SymbolMatcher.normalized(Symbol("_root_.Foo.a."))
+    assert(term.matches(Symbol("_root_.Foo.a#"))) // type
+    assert(term.matches(Symbol("_root_.Foo#a(I)I."))) // method
+    assert(term.matches(Symbol("_root_.Buz.;_root_.Foo.a#"))) // multi
   }
 
 }
