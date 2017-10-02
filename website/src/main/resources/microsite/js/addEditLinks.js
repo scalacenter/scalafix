85cc2b514607acafbe03908c7b900ae835e312c8
Gabriele Petronella <gabriele@buildo.io>
2017-10-02 09:46:03 +0200

    Don't show the edit link when client width <= 500px

--- website/src/main/resources/microsite/js/addEditLinks.js
+++ website/src/main/resources/microsite/js/addEditLinks.js
@@ -1,16 +1,21 @@
 ---
 ---
-const baseUrl = '{{ site.baseurl }}';
-const docsUrl = '{{ site.docsUrl }}';
-let relativePath = window.location.pathname.replace(`${baseUrl}/${docsUrl}/`, '').replace('.html', '.md');
-if (relativePath === '') {
-  relativePath = 'index.md'
+const width = window.document.documentElement.clientWidth;
+// The edit link is too large below this width and it's not useful anyway,
+// as the user is probably on a mobile device.
+if (width > 500) {
+  const baseUrl = '{{ site.baseurl }}';
+  const docsUrl = '{{ site.docsUrl }}';
+  let relativePath = window.location.pathname.replace(`${baseUrl}/${docsUrl}/`, '').replace('.html', '.md');
+  if (relativePath === '') {
+    relativePath = 'index.md'
+  }
+  const repo = '{{ site.githubOwner }}/{{ site.githubRepo }}';
+  const branch = 'microsite';
+  const basePath = `https://github.com/${repo}/edit/${branch}/website/src/main/tut/${docsUrl}`;
+  const target = `${basePath}/${relativePath}`;
+  const editLink = document.createElement('li');
+  const text = 'EDIT THIS PAGE ON GITHUB';
+  editLink.innerHTML = `<a href='${target}' target='_blank'><span>${text}</span></a>`
+  document.querySelector('.nav .pull-right').appendChild(editLink);
 }
-const repo = '{{ site.githubOwner }}/{{ site.githubRepo }}';
-const branch = 'microsite';
-const basePath = `https://github.com/${repo}/edit/${branch}/website/src/main/tut/${docsUrl}`;
-const target = `${basePath}/${relativePath}`;
-const editLink = document.createElement('li');
-const text = 'EDIT THIS PAGE ON GITHUB';
-editLink.innerHTML = `<a href='${target}' target='_blank'><span>${text}</span></a>`
-document.querySelector('.nav .pull-right').appendChild(editLink);
