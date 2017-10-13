c968999e33b474bd744dce66c026ceeee0b2665f
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2017-10-13 08:12:41 +0100

    Added exclude rule to MiMa

--- project/Mima.scala
+++ project/Mima.scala
@@ -4,6 +4,9 @@ object Mima {
   val ignoredABIProblems: Seq[ProblemFilter] = {
     // After v0.5, start running mima checks in CI and document breaking changes here.
     // See https://github.com/typesafehub/migration-manager/wiki/sbt-plugin#basic-usage
-    Seq()
+    Seq(
+      ProblemFilters.exclude[ReversedMissingMethodProblem](
+        "scalafix.patch.PatchOps.replaceSymbols")
+    )
   }
 }
