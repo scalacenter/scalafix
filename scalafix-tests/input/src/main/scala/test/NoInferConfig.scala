6920614ac4d5484462a6326d0759d0dff0511dac
barambani <filippo.mariotti@gmail.com>
2017-10-21 19:27:28 +0100

    Typo

--- scalafix-tests/input/src/main/scala/test/NoInferConfig.scala
+++ scalafix-tests/input/src/main/scala/test/NoInferConfig.scala
@@ -10,7 +10,7 @@ package test
 case object NoInferConfig {
   case class B()
   List(B.apply) // assert: NoInfer.b
-  List[B](B()) // OK, B is not ifferred here
+  List[B](B()) // OK, B is not inferred here
   def sum[A](a: A, b: String): String = { a + b } // assert: NoInfer.any2stringadd
   new Object() + "abc" // assert: NoInfer.any2stringadd
   val x = List(1, "") // OK, the config clears the defaults
