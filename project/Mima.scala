aba4d3b1c62b92404197956570384a6e6d0673eb
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-11-01 18:58:07 +0100

    Exclude binary breaking changes in scalafix-testkit

--- project/Mima.scala
+++ project/Mima.scala
@@ -5,6 +5,14 @@ object Mima {
     // After v0.5, start running mima checks in CI and document breaking changes here.
     // See https://github.com/typesafehub/migration-manager/wiki/sbt-plugin#basic-usage
     Seq(
+      ProblemFilters.exclude[Problem](
+        "scalafix.testkit.DiffAssertion*"),
+      ProblemFilters.exclude[Problem](
+        "scalafix.testkit.SemanticRuleSuite*"),
+      ProblemFilters.exclude[Problem](
+        "scalafix.testkit.SyntacticRuleSuite*"),
+      ProblemFilters.exclude[MissingClassProblem](
+        "scalafix.tests.IntegrationPropertyTest*"),
       ProblemFilters.exclude[ReversedMissingMethodProblem](
         "scalafix.patch.PatchOps.replaceSymbols"),
       ProblemFilters.exclude[ReversedMissingMethodProblem](
