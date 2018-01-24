e035c0e0a4dcd7b3a8b8b131a51d2abb516ae855
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-01-24 17:43:55 +0100

    Please scalafix ExplicitResultTypes
    Very neat to be able to use scalafix for scalafix development!

--- scalafix-core/shared/src/main/scala/scalafix/internal/config/DebugConfig.scala
+++ scalafix-core/shared/src/main/scala/scalafix/internal/config/DebugConfig.scala
@@ -1,12 +1,16 @@
 package scalafix.internal.config
 
 import metaconfig.generic
+import metaconfig.ConfDecoder
+import metaconfig.generic.Surface
 
 case class DebugConfig(
     printSymbols: Boolean = false
 )
 object DebugConfig {
-  implicit val surface = generic.deriveSurface[DebugConfig]
+  implicit val surface: Surface[DebugConfig] =
+    generic.deriveSurface[DebugConfig]
   val default = DebugConfig()
-  implicit val decoder = generic.deriveDecoder[DebugConfig](default)
+  implicit val decoder: ConfDecoder[DebugConfig] =
+    generic.deriveDecoder[DebugConfig](default)
 }
--- scalafix-core/shared/src/main/scala/scalafix/internal/config/NoInferConfig.scala
+++ scalafix-core/shared/src/main/scala/scalafix/internal/config/NoInferConfig.scala
@@ -3,11 +3,13 @@ package scalafix.internal.config
 import metaconfig.ConfDecoder
 import org.langmeta.Symbol
 import metaconfig.generic
+import metaconfig.generic.Surface
 
 case class NoInferConfig(symbols: List[Symbol.Global] = Nil) {}
 
 object NoInferConfig {
-  implicit val surface = generic.deriveSurface[NoInferConfig]
+  implicit val surface: Surface[NoInferConfig] =
+    generic.deriveSurface[NoInferConfig]
   val default: NoInferConfig = NoInferConfig()
   implicit val decoder: ConfDecoder[NoInferConfig] =
     generic.deriveDecoder[NoInferConfig](default)
