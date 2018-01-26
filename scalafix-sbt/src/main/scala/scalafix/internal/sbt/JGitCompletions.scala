c5fa6470a7661c2c72be0aab97b35fac7dcc3b91
Guillaume Massé <masgui@gmail.com>
2018-01-26 23:52:40 +0100

    Fix crashing sbt-scalafix when there is no git repo (fix #575)

--- scalafix-sbt/src/main/scala/scalafix/internal/sbt/JGitCompletions.scala
+++ scalafix-sbt/src/main/scala/scalafix/internal/sbt/JGitCompletions.scala
@@ -1,25 +1,38 @@
 package scalafix.internal.sbt
 
-import java.nio.file.Path
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Constants.DOT_GIT
 import org.eclipse.jgit.lib.RefDatabase
 import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.lib.RepositoryCache
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import org.eclipse.jgit.util.FS
 import org.eclipse.jgit.util.GitDateFormatter
+
+import java.nio.file.Path
 import scala.collection.JavaConverters._
 
 class JGitCompletion(cwd: Path) {
-  private val builder = new FileRepositoryBuilder()
-  private val repo = builder.readEnvironment().setWorkTree(cwd.toFile).build()
-  private val refList = repo.getRefDatabase().getRefs(RefDatabase.ALL).asScala
-  private val git = new Git(repo)
-  private val refs = git.log().setMaxCount(20).call().asScala.toList
-  private val dateFormatter = new GitDateFormatter(
-    GitDateFormatter.Format.RELATIVE)
+  private val isGitRepository = RepositoryCache.FileKey
+    .isGitRepository(cwd.resolve(DOT_GIT).toFile, FS.DETECTED)
+
+  private val (refList, refs) =
+    if (isGitRepository) {
+      val builder = new FileRepositoryBuilder()
+      val repo = builder.readEnvironment().setWorkTree(cwd.toFile).build()
+      val refList0 = repo.getRefDatabase().getRefs(RefDatabase.ALL).asScala
+      val git = new Git(repo)
+      val refs0 = git.log().setMaxCount(20).call().asScala.toList
+      (refList0, refs0)
+    } else {
+      (Nil, Nil)
+    }
 
   val branchesAndTags: List[String] =
     refList.map { case (a, b) => Repository.shortenRefName(a) }.toList
 
+  private val dateFormatter = new GitDateFormatter(
+    GitDateFormatter.Format.RELATIVE)
   val last20Commits: List[(String, String)] =
     refs.map { ref =>
       val relativeCommitTime =
