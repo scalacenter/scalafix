3da338d2b9f8e7f2678bde4b2a8f978cba554442
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-04-05 15:18:50 +0200

    Give up on nio.FileSystem for reading jars.
    Tests were failing with `java.nio.file.NoSuchFileException: /META-INF/semanticdb.semanticidx`
    on 2.11, but not 2.12. I was unable to reproduce the problem using
    `JarFile` from java.io so I'm gonna throw in the towel and use
    JarFile for reading zip files instead.

--- scalafix-core/shared/src/main/scala/scalafix/internal/util/LazySymbolTable.scala
+++ scalafix-core/shared/src/main/scala/scalafix/internal/util/LazySymbolTable.scala
@@ -1,19 +1,14 @@
 package scalafix.internal.util
 
-import java.io.IOException
-import scala.meta.internal.semanticdb3.Scala.Symbols
-import java.net.URI
-import java.nio.file.FileSystem
-import java.nio.file.FileSystemAlreadyExistsException
-import java.nio.file.FileSystems
 import java.nio.file.Files
+import java.util.jar.JarFile
 import org.langmeta.internal.io.PathIO
 import org.langmeta.io.AbsolutePath
 import org.langmeta.io.Classpath
 import scala.collection.concurrent.TrieMap
+import scala.meta.internal.semanticdb3.Scala.Symbols
 import scala.meta.internal.semanticdb3.Scala._
 import scala.meta.internal.{semanticdb3 => s}
-import scala.util.control.NonFatal
 
 /**
   * Implementation of SymbolTable that lazily loads symbols on demand.
@@ -24,10 +19,8 @@ import scala.util.control.NonFatal
 class LazySymbolTable(mclasspath: Classpath) extends SymbolTable {
 
   // Map from symbols to the paths to their corresponding semanticdb file where they are stored.
-  private val notYetLoadedSymbols = TrieMap.empty[String, AbsolutePath]
+  private val notYetLoadedSymbols = TrieMap.empty[String, SemanticdbEntry]
   private val loadedSymbols = TrieMap.empty[String, s.SymbolInformation]
-  private val semanticdb = "META-INF/semanticdb"
-  private val semanticIdx = "META-INF/semanticdb.semanticidx"
 
   mclasspath.shallow.foreach(loadSemanticdbIndex)
 
@@ -47,7 +40,13 @@ class LazySymbolTable(mclasspath: Classpath) extends SymbolTable {
   private def loadSymbolFromClasspath(symbol: String): Unit = {
     val toLoad = notYetLoadedSymbols.get(symbol)
     if (toLoad.isDefined) {
-      val in = toLoad.get.toNIO.toUri.toURL.openStream()
+      val in = toLoad.get match {
+        case Uncompressed(path, uri) =>
+          Files.newInputStream(
+            path.resolve("META-INF").resolve("semanticdb").resolve(uri).toNIO)
+        case Compressed(jar, entry) =>
+          jar.getInputStream(jar.getEntry("META-INF/semanticdb/" + entry))
+      }
       val semanticdb =
         try s.TextDocuments.parseFrom(in)
         finally in.close()
@@ -60,55 +59,43 @@ class LazySymbolTable(mclasspath: Classpath) extends SymbolTable {
     }
   }
 
-  private def loadIndex(root: AbsolutePath): Unit = {
-    val path = root.resolve("META-INF").resolve("semanticdb.semanticidx")
-    if (!Files.isRegularFile(path.toNIO)) return
-    val in = Files.newInputStream(path.toNIO)
-    val index =
-      try s.Index.parseFrom(in)
-      finally in.close()
-    index.toplevels.foreach { toplevel =>
-      notYetLoadedSymbols.put(
-        toplevel.symbol,
-        root.resolve(semanticdb).resolve(toplevel.uri))
-    }
-  }
-
   private def loadSemanticdbIndex(root: AbsolutePath): Unit = {
     if (root.isDirectory) {
-      loadIndex(root)
+      val indexPath = root.resolve("META-INF").resolve("semanticdb.semanticidx")
+      if (indexPath.isFile) {
+        val in = Files.newInputStream(indexPath.toNIO)
+        val index =
+          try s.Index.parseFrom(in)
+          finally in.close()
+        index.toplevels.foreach { toplevel =>
+          notYetLoadedSymbols.put(
+            toplevel.symbol,
+            Uncompressed(root, toplevel.uri))
+        }
+      }
     } else if (PathIO.extension(root.toNIO) == "jar") {
-      withJarFileSystem(root)(loadIndex)
+      val jar = new JarFile(root.toFile)
+      val entry = jar.getEntry("META-INF/semanticdb.semanticidx")
+      if (entry != null) {
+        val in = jar.getInputStream(entry)
+        val index =
+          try s.Index.parseFrom(in)
+          finally in.close()
+        index.toplevels.foreach { toplevel =>
+          notYetLoadedSymbols.put(
+            toplevel.symbol,
+            Compressed(jar, toplevel.uri)
+          )
+        }
+      }
     } else {
       throw new IllegalArgumentException(root.toString())
     }
   }
 
-  private def withJarFileSystem[T](path: AbsolutePath)(
-      f: AbsolutePath => T): T = {
-    val fs = newJarFileSystem(path)
-    try {
-      f(AbsolutePath(fs.getPath("/")))
-    } catch {
-      case NonFatal(e) =>
-        throw new IOException(path.toString, e)
-    }
-  }
-
-  private def newJarFileSystem(path: AbsolutePath): FileSystem = {
-    Files.createDirectories(path.toNIO.getParent)
-    val map = new java.util.HashMap[String, String]()
-    val uri = URI.create("jar:file:" + path.toNIO.toUri.getPath)
-    newFileSystem(uri, map)
-  }
-
-  private def newFileSystem(
-      uri: URI,
-      map: java.util.Map[String, _]): FileSystem =
-    try FileSystems.newFileSystem(uri, map)
-    catch {
-      case _: FileSystemAlreadyExistsException =>
-        FileSystems.getFileSystem(uri)
-    }
-
+  private sealed trait SemanticdbEntry
+  private case class Uncompressed(root: AbsolutePath, uri: String)
+      extends SemanticdbEntry
+  private case class Compressed(jarFile: JarFile, entry: String)
+      extends SemanticdbEntry
 }
