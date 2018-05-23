8bc4c438a039bad16d8ad9eb24492f0ff969e051
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-05-23 15:25:34 +0200

    Refactor

--- scalafix-cli/src/main/scala/scalafix/v1/Args.scala
+++ scalafix-cli/src/main/scala/scalafix/v1/Args.scala
@@ -18,56 +18,17 @@ import metaconfig.annotation.ExtraName
 import metaconfig.generic.Surface
 import metaconfig.internal.ConfGet
 import metaconfig.typesafeconfig.typesafeConfigMetaconfigParser
-import org.langmeta.internal.io.FileIO
-import org.langmeta.io.RelativePath
-import scala.meta.Input
-import scala.meta.Source
 import scala.meta.internal.io.PathIO
 import scala.meta.io.AbsolutePath
 import scala.meta.io.Classpath
-import scala.meta.parsers.Parsed
 import scalafix.cli.CliRunner
 import scalafix.internal.cli.ClasspathOps
-import scalafix.internal.cli.WriteMode
 import scalafix.internal.config.OutputFormat
 import scalafix.internal.config.ScalafixConfig
 import scalafix.internal.util.ClassloadRule
 import scalafix.internal.util.SymbolTable
 import scalafix.internal.v1.Rules
 import scalafix.reflect.ScalafixReflectV1
-
-case class ValidatedArgs(
-    args: Args,
-    symtab: SymbolTable,
-    rules: Rules,
-    config: ScalafixConfig,
-    classpath: Classpath,
-    sourceroot: AbsolutePath,
-    pathReplace: AbsolutePath => AbsolutePath
-) {
-  import args._
-
-  val mode: WriteMode =
-    // TODO: suppress
-    if (stdout) WriteMode.Stdout
-    else if (test) WriteMode.Test
-    else WriteMode.WriteFile
-
-  def input(file: AbsolutePath): Input = {
-    Input.VirtualFile(file.toString(), FileIO.slurp(file, charset))
-  }
-
-  def parse(input: Input): Parsed[Source] = {
-    import scala.meta._
-    val dialect = parser.dialectForFile(input.syntax)
-    dialect(input).parse[Source]
-  }
-
-  def matches(path: RelativePath): Boolean =
-    Args.baseMatcher.matches(path.toNIO) &&
-      args.exclude.forall(!_.matches(path.toNIO))
-
-}
 
 case class Args(
     cwd: AbsolutePath,
---
+++ scalafix-cli/src/main/scala/scalafix/v1/ValidatedArgs.scala
@@ -0,0 +1,46 @@
+package scalafix.v1
+
+import org.langmeta.internal.io.FileIO
+import org.langmeta.io.RelativePath
+import scala.meta.Input
+import scala.meta.AbsolutePath
+import scala.meta.Classpath
+import scala.meta.Source
+import scala.meta.parsers.Parsed
+import scalafix.internal.cli.WriteMode
+import scalafix.internal.config.ScalafixConfig
+import scalafix.internal.util.SymbolTable
+import scalafix.internal.v1.Rules
+
+case class ValidatedArgs(
+    args: Args,
+    symtab: SymbolTable,
+    rules: Rules,
+    config: ScalafixConfig,
+    classpath: Classpath,
+    sourceroot: AbsolutePath,
+    pathReplace: AbsolutePath => AbsolutePath
+) {
+  import args._
+
+  val mode: WriteMode =
+    // TODO: suppress
+    if (stdout) WriteMode.Stdout
+    else if (test) WriteMode.Test
+    else WriteMode.WriteFile
+
+  def input(file: AbsolutePath): Input = {
+    Input.VirtualFile(file.toString(), FileIO.slurp(file, charset))
+  }
+
+  def parse(input: Input): Parsed[Source] = {
+    import scala.meta._
+    val dialect = parser.dialectForFile(input.syntax)
+    dialect(input).parse[Source]
+  }
+
+  def matches(path: RelativePath): Boolean =
+    Args.baseMatcher.matches(path.toNIO) &&
+      args.exclude.forall(!_.matches(path.toNIO))
+
+}
