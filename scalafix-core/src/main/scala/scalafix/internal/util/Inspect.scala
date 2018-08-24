75143c5fb103bfea0efbd058a7cce27a02790d32
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-08-24 17:18:13 +0200

    Refactor Inspect implementation a bit

--- scalafix-core/src/main/scala/scalafix/internal/util/Inspect.scala
+++ scalafix-core/src/main/scala/scalafix/internal/util/Inspect.scala
@@ -1,108 +1,79 @@
-package structure
+package scalafix.internal.util
 
+import scala.meta.Tree
 import scala.meta._
 import org.typelevel.paiges._
 
-import scala.language.implicitConversions
-
-trait Inspect {
-  def pretty(input: CanPretty, showFieldNames: Boolean = false): String = {
-    input match {
-      case TreeCanPretty(tree)       => prettyTree(tree, showFieldNames)
-      case ListTreeCanPretty(tree)   => prettyList(tree, showFieldNames)
-      case OptionTreeCanPretty(tree) => prettyOption(tree, showFieldNames)
-    }
+object Inspect {
+  def pretty(tree: Tree, showFieldNames: Boolean): String = {
+    prettyTree(tree, showFieldNames).render(1)
   }
 
-  // workarround to for polymorphic methods with default parameter
-  case class TreeCanPretty(tree: Tree) extends CanPretty
-  case class ListTreeCanPretty(tree: List[Tree]) extends CanPretty
-  case class OptionTreeCanPretty(tree: Option[Tree]) extends CanPretty
-
-  sealed trait CanPretty
-  object CanPretty {
-    implicit def treeCanPretty[T <: Tree](tree: T): CanPretty = TreeCanPretty(tree)
-    implicit def listTreeCanPretty(tree: List[Tree]): CanPretty = ListTreeCanPretty(tree)
-    implicit def optionTreeCanPretty(tree: Option[Tree]): CanPretty = OptionTreeCanPretty(tree)    
+  private def prettyList(tree: List[Tree], showFieldNames: Boolean): Doc = {
+    wrapList(tree.map(t => prettyTree(t, showFieldNames)))
   }
 
-  private def prettyTree(tree: Tree, showFieldNames: Boolean): String = {
-    prettyDoc(tree, showFieldNames).render(1)
-  }
-
-  private def prettyList(tree: List[Tree], showFieldNames: Boolean): String = {
-    prettyDoc(tree, showFieldNames).render(1)
-  }
-
-  private def prettyOption(tree: Option[Tree], showFieldNames: Boolean): String = {
-    prettyDoc(tree, showFieldNames).render(1)
-  }
-
-  private def prettyDoc(tree: List[Tree], showFieldNames: Boolean): Doc = {
-    wrapList(tree.map(t => prettyDoc(t, showFieldNames)))
-  }
-
-  private def prettyDoc(tree: Option[Tree], showFieldNames: Boolean): Doc = {
-    wrapOption(tree.map(t => prettyDoc(t, showFieldNames)))
-  }
-
-  private def prettyDoc(tree: Tree, showFieldNames: Boolean): Doc = {
+  private def prettyTree(tree: Tree, showFieldNames: Boolean): Doc = {
     tree match {
-      case _ if tree.tokens.isEmpty => Doc.empty
-      case v: Term.Name             => Doc.text(v.structure)
-      case t: Type.Name             => Doc.text(t.structure)
-      case _ => {
-        val args =
-          tree.productFields.zip(tree.productIterator.toList).map {
-            case (k, v) =>
-              val rhs =
-                v match {
-                  case v: Term.Name => Doc.text(v.structure)
-                  case t: Tree      => prettyDoc(t, showFieldNames)
-                  case o: Option[_] =>
-                    o match {
-                      case Some(t: Tree) =>
-                        wrap(
-                          Doc.text("Some") + Doc.char('('),
-                          List(prettyDoc(t, showFieldNames)),
-                          Doc.char(')')
-                        )
-                      case None => Doc.text("None")
-                      case _    => throw new Exception("cannot handle: " + o)
-                    }
-                  case vs: List[_] =>
-                    vs match {
-                      case Nil => Doc.text("Nil")
-                      case (h: Tree) :: _ => {
-                        prettyDoc(vs.asInstanceOf[List[Tree]], showFieldNames)
-                      }
-                      case (h: List[_]) :: _ => {
-                        val vsT = vs.asInstanceOf[List[List[Tree]]]
-                        wrapList(vsT.map(v => prettyDoc(v, showFieldNames)))
-                      }
-                      case _ => throw new Exception("cannot handle: " + vs)
-                    }
-                  case _ => Doc.text(v.toString)
+      case _ if tree.tokens.isEmpty =>
+        Doc.empty
+      case v: Term.Name =>
+        Doc.text(v.structure)
+      case t: Type.Name =>
+        Doc.text(t.structure)
+      case _ =>
+        val args = tree.productFields.zip(tree.productIterator.toList).map {
+          case (fieldName, value) =>
+            val rhs = value match {
+              case v: Term.Name => Doc.text(v.structure)
+              case t: Tree => prettyTree(t, showFieldNames)
+              case o: Option[_] =>
+                o match {
+                  case Some(t: Tree) =>
+                    wrap(
+                      Doc.text("Some") + Doc.char('('),
+                      List(prettyTree(t, showFieldNames)),
+                      Doc.char(')')
+                    )
+                  case None =>
+                    Doc.text("None")
+                  case _ =>
+                    throw new Exception("cannot handle: " + o)
                 }
+              case vs: List[_] =>
+                vs match {
+                  case Nil => Doc.text("Nil")
+                  case (_: Tree) :: _ =>
+                    prettyList(vs.asInstanceOf[List[Tree]], showFieldNames)
+                  case (_: List[_]) :: _ =>
+                    val vsT = vs.asInstanceOf[List[List[Tree]]]
+                    wrapList(vsT.map(v => prettyList(v, showFieldNames)))
+                  case _ =>
+                    throw new IllegalArgumentException("cannot handle: " + vs)
+                }
+              case _ => Doc.text(value.toString)
+            }
 
-              if (showFieldNames) Doc.text(k) + Doc.text(" = ") + rhs
-              else rhs
-          }
+            if (showFieldNames) Doc.text(fieldName) + Doc.text(" = ") + rhs
+            else rhs
+        }
 
         wrap(Doc.text(tree.productPrefix) + Doc.char('('), args, Doc.char(')'))
-      }
     }
   }
 
   private def wrapList(args: List[Doc]): Doc = {
-    if (args.nonEmpty) wrap(Doc.text("List") + Doc.char('('), args, Doc.char(')'))
-    else Doc.text("Nil")
+    if (args.nonEmpty) {
+      wrap(Doc.text("List") + Doc.char('('), args, Doc.char(')'))
+    } else {
+      Doc.text("Nil")
+    }
   }
 
   private def wrapOption(opt: Option[Doc]): Doc = {
     opt match {
       case Some(doc) => Doc.text("Some") + Doc.char('(') + doc + Doc.char(')')
-      case None      => Doc.text("None")
+      case None => Doc.text("None")
     }
   }
 
--- scalafix-core/src/test/scala/scalafix/tests/util/InspectSuite.scala
+++ scalafix-core/src/test/scala/scalafix/tests/util/InspectSuite.scala
@@ -1,40 +1,40 @@
-package structure
+package scalafix.tests.util
 
 import scala.meta._
-import utest._
+import org.scalatest.FunSuite
+import scalafix.testkit.DiffAssertions
+import scalafix.v1._
 
-object InspectSuite extends TestSuite{
-  val tests = Tests{
-    "pretty(t)" - {
-      val obtained = pretty(q"a.b.c.d")
-      val expected = 
-        """|Term.Select(
-           |  Term.Select(
-           |    Term.Select(
-           |      Term.Name("a"),
-           |      Term.Name("b")
-           |    ),
-           |    Term.Name("c")
-           |  ),
-           |  Term.Name("d")
-           |)""".stripMargin
-      assert(obtained == expected)
-    }
+class InspectSuite extends FunSuite with DiffAssertions {
+  test("pretty(t)") {
+    val obtained = q"a.b.c.d".inspect
+    val expected =
+      """|Term.Select(
+         |  Term.Select(
+         |    Term.Select(
+         |      Term.Name("a"),
+         |      Term.Name("b")
+         |    ),
+         |    Term.Name("c")
+         |  ),
+         |  Term.Name("d")
+         |)""".stripMargin
+    assert(obtained == expected)
+  }
 
-    "pretty(t, showFieldNames = true)" - {
-        val obtained = pretty(q"a.b.c.d", showFieldNames = true)
-        val expected = 
-          """|Term.Select(
-             |  qual = Term.Select(
-             |    qual = Term.Select(
-             |      qual = Term.Name("a"),
-             |      name = Term.Name("b")
-             |    ),
-             |    name = Term.Name("c")
-             |  ),
-             |  name = Term.Name("d")
-             |)""".stripMargin
-        assert(obtained == expected)
-    }
+  test("pretty(t, showFieldNames = true)") {
+    val obtained = q"a.b.c.d".inspectLabeled
+    val expected =
+      """|Term.Select(
+         |  qual = Term.Select(
+         |    qual = Term.Select(
+         |      qual = Term.Name("a"),
+         |      name = Term.Name("b")
+         |    ),
+         |    name = Term.Name("c")
+         |  ),
+         |  name = Term.Name("d")
+         |)""".stripMargin
+    assert(obtained == expected)
   }
 }
