2ba23057794bfd8d3413a09f1c7a7cea764400b3
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-20 14:37:03 +0200

    Deploy website
    Deploy website version based on 182806a13840e54c8ea624e5934f7423e7d4d904

--- v0.5/_site/js/addSectionLinks.js
+++
@@ -1,9 +0,0 @@
-$("h2, h3, h4, h5, h6").each(function(i, el) {
-  var $el, icon, id;
-  $el = $(el);
-  id = $el.attr('id');
-  icon = '<i class="fa fa-link"></i>';
-  if (id) {
-    return $el.prepend($("<a />").addClass("header-anchor-link").attr("href", "#" + id).html(icon));
-  }
-});
--- v0.5/js/setCallToAction.js
+++
@@ -1,5 +0,0 @@
-const ctaButton = document.querySelector('.jumbotron .container p.text-center a');
-if (ctaButton) {
-  ctaButton.innerHTML = 'Get started';
-  ctaButton.setAttribute('href', 'docs/users/installation');
-}
