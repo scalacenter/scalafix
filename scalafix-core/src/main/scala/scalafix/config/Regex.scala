ff2483213cd689620cac7ad3a1e5c6cad0aaa38c
Jacob Barber <jacob.barber@bamtechmedia.com>
2018-11-13 19:00:31 -0500

    fmt

--- scalafix-core/src/main/scala/scalafix/config/Regex.scala
+++ scalafix-core/src/main/scala/scalafix/config/Regex.scala
@@ -6,17 +6,18 @@ import java.util.regex.Pattern
 import scalafix.internal.config.ScalafixMetaconfigReaders._
 
 class Regex(
-  val pattern: Pattern,
-  val captureGroup: Option[Int]
+    val pattern: Pattern,
+    val captureGroup: Option[Int]
 )
 
 object Regex {
   implicit val regexDecoder: ConfDecoder[Regex] =
     ConfDecoder.instance[Regex] {
       case obj: Conf.Obj =>
-        (obj.get[Pattern]("pattern") |@| obj.getOption[Int]("captureGroup")).map {
-          case (pattern, groupIndex) => new Regex(pattern, groupIndex)
-        }
+        (obj.get[Pattern]("pattern") |@| obj.getOption[Int]("captureGroup"))
+          .map {
+            case (pattern, groupIndex) => new Regex(pattern, groupIndex)
+          }
     }
 
   implicit val customMessageRegexDecoder: ConfDecoder[CustomMessage[Regex]] =
