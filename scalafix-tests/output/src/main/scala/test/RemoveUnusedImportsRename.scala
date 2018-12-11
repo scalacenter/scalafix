3221958d8beb51026ab30f3df6ef80f298006892
Marcelo Cenerino <marcelocenerine@gmail.com>
2018-12-11 08:38:26 +0000

    add tests back

--- scalafix-tests/output/src/main/scala/test/RemoveUnusedImportsRename.scala
+++ scalafix-tests/output/src/main/scala/test/RemoveUnusedImportsRename.scala
@@ -1,7 +1,78 @@
 package test
 
-import scala.util.{Success => _, _}
+package t1 {
+  import scala.collection.immutable.{List => IList}
 
-class RemoveUnusedImportsRename {
-  Failure(new Exception())
+  object RenameUsed {
+    IList.empty[String]
+  }
+}
+
+package t2 {
+
+  object RenameUnused
+}
+
+package t3 {
+  import scala.util.{Success => _, _}
+
+  object RenameUnusedWithUsedWildcard {
+    Failure(new Exception())
+  }
+}
+
+package t4 {
+
+  object RenameUnusedWithUnusedWildcard
+}
+
+package t5 {
+
+  object MultipleUnusedRenames
+}
+
+package t6 {
+  import scala.concurrent.duration._
+
+  object UnusedRenameFollowedByUsedWildcardInDifferentImports {
+    Duration("10s")
+  }
+}
+
+package t7 {
+  import scala.io.StdIn._
+
+  object UnusedRenameFollowingUsedWildcardInDifferentImports {
+    lazy val l = readLine()
+  }
+}
+
+package t8 {
+  import scala.io._
+
+  object UnusedRenameFollowedByUsedWildcardBetweenCommas {
+    val c = Codec.UTF8
+  }
+}
+
+package t9 {
+  import java.util
+  import util.concurrent
+  import concurrent.atomic
+  import atomic._
+
+  object UnusedRenameWithUsedWildcardWhosePackagesAreImportedIndividually {
+    new AtomicBoolean()
+  }
+}
+
+package t10 {
+  import java.{util => jutil}
+  import jutil.{concurrent => jconcurrent}
+  import jconcurrent.{locks => jlocks}
+  import jlocks._
+
+  object UnusedRenameWithUsedWildcardWhosePackagesAreImportedIndividuallyAndRenamed {
+    new StampedLock()
+  }
 }
