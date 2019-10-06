2079c29bfcaab0bcf00582fd4dc3e0d6f65617cc
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-10-06 10:34:57 +0200

    Fail to reproduce bug around stable identifier

--- scalafix-tests/input/src/main/scala/tests/ExplicitResultTypesBug.scala
+++ scalafix-tests/input/src/main/scala/tests/ExplicitResultTypesBug.scala
@@ -5,10 +5,16 @@ package tests
 
 import scala.collection.{Seq => SSeq}
 import java.lang.{Boolean => JBoolean}
+import scala.reflect.runtime.universe._
 
 object ExplicitResultTypesBug {
   type Seq = Int
   def foo(a: Int*) = a
   def foo = JBoolean.TRUE
+
+  class MyMirror(owner: ClassMirror) {
+    val symbol =
+      owner.symbol.info.decl(TermName("")).asMethod
+  }
 }
 
--- scalafix-tests/output/src/main/scala/tests/ExplicitResultTypesBug.scala
+++ scalafix-tests/output/src/main/scala/tests/ExplicitResultTypesBug.scala
@@ -2,10 +2,16 @@ package tests
 
 import scala.collection.{Seq => SSeq}
 import java.lang.{Boolean => JBoolean}
+import scala.reflect.runtime.universe._
 
 object ExplicitResultTypesBug {
   type Seq = Int
   def foo(a: Int*): SSeq[Int] = a
   def foo: JBoolean = JBoolean.TRUE
+
+  class MyMirror(owner: ClassMirror) {
+    val symbol: reflect.runtime.universe.MethodSymbol =
+      owner.symbol.info.decl(TermName("")).asMethod
+  }
 }
 
