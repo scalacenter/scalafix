4d19177524bdc8bec7c5a255a9673a9b66379fc8
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-03-18 11:46:14 +0000

    Update test-release script

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -3,14 +3,14 @@ set -eux
 
 version=$1
 
-coursier fetch \
+coursier resolve \
   ch.epfl.scala:scalafix-interfaces:$version  \
   ch.epfl.scala:scalafix-core_2.11:$version  \
   ch.epfl.scala:scalafix-core_2.12:$version  \
   ch.epfl.scala:scalafix-reflect_2.11.12:$version  \
-  ch.epfl.scala:scalafix-reflect_2.12.8:$version  \
+  ch.epfl.scala:scalafix-reflect_2.12.11:$version  \
   ch.epfl.scala:scalafix-cli_2.11.12:$version  \
-  ch.epfl.scala:scalafix-cli_2.12.8:$version  \
+  ch.epfl.scala:scalafix-cli_2.12.11:$version  \
   ch.epfl.scala:scalafix-testkit_2.11.12:$version  \
-  ch.epfl.scala:scalafix-testkit_2.12.8:$version \
-  -r sonatype:releases
+  ch.epfl.scala:scalafix-testkit_2.12.11:$version \
+  -r sonatype:public
