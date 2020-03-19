bf60013c7370a4003834e37b4451736d51e3118b
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-03-19 13:42:43 +0000

    Migrate most tests to GH Actions for CI

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 /* scalafmt: { maxColumn = 120 }*/
 
 object Dependencies {
-  val scalametaV = "4.3.6"
+  val scalametaV = "4.3.7"
   val metaconfigV = "0.9.9"
   def scala210 = "2.10.7"
   def scala211 = "2.11.12"
