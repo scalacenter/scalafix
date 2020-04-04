558ea9488a4e13a6facc941dab5507c8be591f04
Bùi Việt Thành <thanhbv@sandinh.net>
2020-04-04 14:44:55 +0700

    update scala-xml 1.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   def testsDeps = List(
     // integration property tests
     "io.get-coursier" %% "coursier" % coursierV,
-    "org.scala-lang.modules" %% "scala-xml" % "1.2.0",
+    "org.scala-lang.modules" %% "scala-xml" % "1.3.0",
     "org.typelevel" %% "cats-core" % "2.0.0",
     "com.typesafe.slick" %% "slick" % "3.3.2",
     "com.chuusai" %% "shapeless" % "2.3.3",
