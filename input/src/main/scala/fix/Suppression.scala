5d767aa2ea82d2b0c648888fc9be689c7b077345
Cheng Lian <lian@databricks.com>
2020-04-15 14:55:49 -0700

    Add a test case for suppression

---
+++ input/src/main/scala/fix/Suppression.scala
@@ -0,0 +1,16 @@
+/*
+rules = OrganizeImports
+OrganizeImports.groups = ["re:javax?\\.", "*", "scala."]
+ */
+
+package fix
+
+// scalafix:off
+import java.time.Clock
+import scala.collection.JavaConverters._
+import sun.misc.BASE64Encoder
+import scala.concurrent.ExecutionContext
+import javax.annotation.Generated
+// scalafix:on
+
+object Suppression
---
+++ output/src/main/scala/fix/Suppression.scala
@@ -0,0 +1,11 @@
+package fix
+
+// scalafix:off
+import java.time.Clock
+import scala.collection.JavaConverters._
+import sun.misc.BASE64Encoder
+import scala.concurrent.ExecutionContext
+import javax.annotation.Generated
+// scalafix:on
+
+object Suppression
