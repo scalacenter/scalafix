72b9590bb425b7d1b6054709fc06f8346d7548ab
Cheng Lian <lian@databricks.com>
2020-05-03 22:19:35 -0700

    Fix comment typo

--- rules/src/main/scala/fix/OrganizeImports.scala
+++ rules/src/main/scala/fix/OrganizeImports.scala
@@ -183,9 +183,9 @@ class OrganizeImports(config: OrganizeImportsConfig) extends SemanticRule("Organ
         importeesSorted sortBy (_.syntax)
 
       case SymbolsFirst =>
-        // Hack: This is a quick-n-dirty way to achieve a the import ordering provided by the
-        // IntelliJ IDEA Scala plugin. This implementation does not cover cases like quoted
-        // identifiers containg "._" and/or braces.
+        // HACK: This is a quick-n-dirty way to achieve the import ordering provided by the IntelliJ
+        // IDEA Scala plugin. This implementation does not cover cases like quoted identifiers
+        // containg "._" and/or braces.
         importeesSorted sortBy {
           _.syntax
             .replaceAll("\\._$", ".\0")
