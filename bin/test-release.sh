a8ad15cde6a8cb6327e434ab5c380ee178bd32ba
Brice Jaglin <bjaglin@teads.tv>
2020-07-13 19:19:08 +0200

    support for Scala 2.12.12

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -9,12 +9,12 @@ cs resolve \
   ch.epfl.scala:scalafix-core_2.12:$version  \
   ch.epfl.scala:scalafix-core_2.13:$version  \
   ch.epfl.scala:scalafix-reflect_2.11.12:$version  \
-  ch.epfl.scala:scalafix-reflect_2.12.11:$version  \
+  ch.epfl.scala:scalafix-reflect_2.12.12:$version  \
   ch.epfl.scala:scalafix-reflect_2.13.3:$version  \
   ch.epfl.scala:scalafix-cli_2.11.12:$version  \
-  ch.epfl.scala:scalafix-cli_2.12.11:$version  \
+  ch.epfl.scala:scalafix-cli_2.12.12:$version  \
   ch.epfl.scala:scalafix-cli_2.13.3:$version  \
   ch.epfl.scala:scalafix-testkit_2.11.12:$version  \
-  ch.epfl.scala:scalafix-testkit_2.12.11:$version \
+  ch.epfl.scala:scalafix-testkit_2.12.12:$version \
   ch.epfl.scala:scalafix-testkit_2.13.3:$version \
   -r sonatype:public
