176776f772f74b725bdc392148c1423af9c7efff
Meriam Lachkar <meriam.lachkar@gmail.com>
2020-10-08 10:38:59 +0200

    Handle secondary constructor in Procedur syntax
    * code inspired from [ConstructorProcedureSyntax](https://github.com/ohze/scala-rewrites/blob/dotty/rewrites/src/main/scala/fix/scala213/ConstructorProcedureSyntax.scala)
    * We don't remove braces in case of 1 statement
    
    fixes #1235

--- scalafix-rules/src/main/scala/scalafix/internal/rule/ProcedureSyntax.scala
+++ scalafix-rules/src/main/scala/scalafix/internal/rule/ProcedureSyntax.scala
@@ -1,6 +1,9 @@
 package scalafix.internal.rule
 
 import scala.meta._
+import scala.meta.tokens.Token
+import scala.meta.tokens.Token.Equals
+import scala.meta.tokens.Token.RightParen
 
 import scalafix.util.Trivia
 import scalafix.v1._
@@ -9,6 +12,7 @@ class ProcedureSyntax extends SyntacticRule("ProcedureSyntax") {
 
   override def description: String =
     "Replaces deprecated procedure syntax with explicit ': Unit ='"
+
   override def isRewrite: Boolean = true
 
   override def fix(implicit doc: SyntacticDocument): Patch = {
@@ -23,6 +27,21 @@ class ProcedureSyntax extends SyntacticRule("ProcedureSyntax") {
           toAdd <- doc.tokenList.leading(bodyStart).find(!_.is[Trivia])
         } yield Patch.addRight(toAdd, s": Unit =").atomic
         fixed.getOrElse(Patch.empty)
+
+      /** @see [[https://github.com/ohze/scala-rewrites/blob/dotty/rewrites/src/main/scala/fix/scala213/ConstructorProcedureSyntax.scala ConstructorProcedureSyntax.scala]] */
+      case t: Ctor.Secondary =>
+        val tokens = t.tokens
+        val beforeInitIdx = tokens.indexOf(t.init.tokens.head) - 1
+        // last RightParen before init
+        val lastRightParenIdx =
+          tokens.lastIndexWhere(_.is[RightParen], beforeInitIdx)
+        // if slicedTokens don't have Equals token => need patching
+        val slicedTokens = tokens.slice(lastRightParenIdx, beforeInitIdx)
+        slicedTokens.find(_.is[Equals]) match {
+          case Some(_) => Patch.empty
+          case None => Patch.addRight(tokens(lastRightParenIdx), " =")
+        }
+
     }.asPatch
   }
 }
