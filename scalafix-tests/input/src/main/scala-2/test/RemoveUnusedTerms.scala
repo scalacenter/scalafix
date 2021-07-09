000ab90e15509837dbe836e45f1cc5bb0b0c1c21
Taisuke Oe <oeuia.t@gmail.com>
2021-07-09 17:56:10 +0900

    add anonymous function parameter support & enhance patvar RemoveUnused

--- scalafix-tests/input/src/main/scala-2/test/RemoveUnusedTerms.scala
+++ scalafix-tests/input/src/main/scala-2/test/RemoveUnusedTerms.scala
@@ -19,4 +19,18 @@ object RemoveUnusedTerms {
   val dd = 0
   def f(x: Int) = "unused"
   private def ff(x: Int) = "unused"
+
+  def g(x: String => Unit): Unit = ???
+  g{implicit string => println("g")}
+  def pf(x: PartialFunction[Any, Unit]): Unit = ???
+  case class A(a: Int)
+  pf{
+    case string: String => ???
+    case (i: Int) => ???
+    case (a: Int, b) => println(b)
+    case a@A(v) => ???
+    case x :: (y1, y2) :: Nil => println(x)
+    case (zz) => ???
+  }
+  try ??? catch {case e: Exception => ???}
 }
