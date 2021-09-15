02d34ce0ee4e6dabdae700bc90a9044656076587
Scala Steward <me@scala-steward.org>
2021-09-15 02:11:07 +0200

    Update scala-compiler, scala-library, ... to 2.12.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ import scala.util.Try
 
 object Dependencies {
   val scala211 = "2.11.12"
-  val scala212 = "2.12.14"
+  val scala212 = "2.12.15"
   val scala213 = "2.13.6"
   val scala3 = "3.0.2"
   // we support 3 last binary versions of scala212 and scala213
