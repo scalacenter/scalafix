45a8d9c200404b23bce88f157d6416b2cd25ce17
Brice Jaglin <bjaglin@gmail.com>
2022-02-14 13:40:36 +0100

    fix docs: reflect new param signature after 1547da5

--- scalafix-rules/src/main/scala-2.12+/scalafix/internal/rule/TPrintImplicits.scala
+++ scalafix-rules/src/main/scala-2.12+/scalafix/internal/rule/TPrintImplicits.scala
@@ -4,10 +4,12 @@ import java.util.regex.Pattern
 
 import metaconfig.pprint._
 import scalafix.config.CustomMessage
+import scalafix.config.Regex
 
 class TPrintImplicits {
-  implicit val tprintPattern: TPrint[List[CustomMessage[Pattern]]] =
-    new TPrint[List[CustomMessage[Pattern]]] {
+  implicit val tprintPattern
+      : TPrint[List[CustomMessage[Either[Regex, Pattern]]]] =
+    new TPrint[List[CustomMessage[Either[Regex, Pattern]]]] {
       def render(implicit tpc: TPrintColors): fansi.Str =
         fansi.Str("List[Regex]")
     }
