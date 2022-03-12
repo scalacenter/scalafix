51d2ca2eef527da7ec10bfe8eb5681abe24b74ec
Brice Jaglin <bjaglin@gmail.com>
2022-03-12 18:20:56 +0100

    coursier 2.0.1-M5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
 
   val bijectionCoreV = "0.9.7"
   val collectionCompatV = "2.6.0"
-  val coursierV = "2.0.0-RC5-6"
+  val coursierV = "2.1.0-M5"
   val coursierInterfaceV = "1.0.6"
   val commontTextV = "1.9"
   val googleDiffV = "1.3.0"
