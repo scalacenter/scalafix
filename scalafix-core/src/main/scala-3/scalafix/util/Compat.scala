a0b0aeb787b5e7d489dbf4f8ab13944f16bd7d51
“Razvan <razvan.vacaru@tmnl.nl>
2022-07-08 15:55:57 +0200

    introduced scala3 scalafix util w/Compat class, fix TokenList partially

---
+++ scalafix-core/src/main/scala-3/scalafix/util/Compat.scala
@@ -0,0 +1,9 @@
+package scalafix.util
+
+import scala.collection.immutable.IndexedSeq
+
+object Compat {
+  type View[T] = collection.SeqView[T] //, IndexedSeq[T]]
+  type SeqView[T] = collection.SeqView[T] //, IndexedSeq[T]]
+
+}
