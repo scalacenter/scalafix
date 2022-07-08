12198d17dca8d519791ecb2971589aebc184b5b2
“Razvan <razvan.vacaru@tmnl.nl>
2022-07-08 16:53:01 +0200

    (chore): )removed comments after clarification on Compat

--- scalafix-core/src/main/scala-3/scalafix/util/Compat.scala
+++ scalafix-core/src/main/scala-3/scalafix/util/Compat.scala
@@ -3,7 +3,7 @@ package scalafix.util
 import scala.collection.immutable.IndexedSeq
 
 object Compat {
-  type View[T] = collection.SeqView[T] //, IndexedSeq[T]]
-  type SeqView[T] = collection.SeqView[T] //, IndexedSeq[T]]
+  type View[T] = collection.SeqView[T]
+  type SeqView[T] = collection.SeqView[T]
 
 }
