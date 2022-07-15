6c4bf8f763a9b2250494f0273e803b17e9dd1d62
“Razvan <razvan.vacaru@tmnl.nl>
2022-07-15 18:03:11 +0200

    rename of new compaSenticType classes/files

--- scalafix-core/src/main/scala-3/scalafix/util/CompatSemanticType.scala
+++
@@ -1,9 +0,0 @@
-package scalafix.util
-
-
-import scala.meta.internal.semanticdb.XtensionSemanticdbType
-import scala.meta.internal.semanticdb.XtensionSemanticdbScope
-object CompatSemanticType {
-  type SemanticdbType = XtensionSemanticdbType
-  type SemanticdbScope = XtensionSemanticdbScope
-}
