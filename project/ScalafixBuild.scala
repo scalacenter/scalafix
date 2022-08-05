c3456aa9e01daa436c19eb5dd15afbe7507f89ff
“Razvan <razvan.vacaru@tmnl.nl>
2022-08-05 16:13:09 +0200

    Squashed commits: scalafix-testkit3 cross compiled for scala3
    This is the commit message #2:
    
    build sbt changes for test modules
    
    This is the commit message #3:
    
    test modules: scalatest3V definition

--- project/ScalafixBuild.scala
+++ project/ScalafixBuild.scala
@@ -146,7 +146,8 @@ object ScalafixBuild extends AutoPlugin with GhpagesKeys {
         "core3/compile" ::
         "rules3/compile" ::
         "reflect3/compile" ::
-        "cli3/compile" :: // TODO replace core rules and reflect with unit3 in the end
+        "cli3/compile" ::
+        "testkit3/compile" :: // TODO replace core rules and reflect with unit3 in the end
         s
     },
     commands += Command.command("ci-213") { s =>
