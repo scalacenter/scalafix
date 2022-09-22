2a3ac8efb41407d3fbae90d951e5276a9a809778
Brice Jaglin <bjaglin@gmail.com>
2022-09-22 23:37:27 +0200

    RuleCompiler: use java.io.File when requesting output
    Using scala.reflect.io in Scala 3 is a recipe for bugs as we should be
    using the equivalent dotty classes instead

--- scalafix-reflect/src/main/scala-2/scalafix/internal/reflect/RuleCompiler.scala
+++ scalafix-reflect/src/main/scala-2/scalafix/internal/reflect/RuleCompiler.scala
@@ -1,9 +1,12 @@
 package scalafix.internal.reflect
+import java.io.File
+
 import scala.reflect.internal.util.AbstractFileClassLoader
 import scala.reflect.internal.util.BatchSourceFile
+import scala.reflect.io.Directory
+import scala.reflect.io.PlainDirectory
 import scala.tools.nsc.Global
 import scala.tools.nsc.Settings
-import scala.tools.nsc.io.AbstractFile
 import scala.tools.nsc.io.VirtualDirectory
 import scala.tools.nsc.reporters.StoreReporter
 
@@ -14,17 +17,21 @@ import metaconfig.Position
 
 class RuleCompiler(
     classpath: String,
-    target: AbstractFile = new VirtualDirectory("(memory)", None)
+    targetDirectory: Option[File] = None
 ) {
+  private val output = targetDirectory match {
+    case Some(file) => new PlainDirectory(new Directory(file))
+    case None => new VirtualDirectory("(memory)", None)
+  }
   private val settings = new Settings()
   settings.deprecation.value = true // enable detailed deprecation warnings
   settings.unchecked.value = true // enable detailed unchecked warnings
-  settings.outputDirs.setSingleOutput(target)
+  settings.outputDirs.setSingleOutput(output)
   settings.classpath.value = classpath
   lazy val reporter = new StoreReporter
   private val global = new Global(settings, reporter)
   private val classLoader =
-    new AbstractFileClassLoader(target, this.getClass.getClassLoader)
+    new AbstractFileClassLoader(output, this.getClass.getClassLoader)
 
   def compile(input: Input): Configured[ClassLoader] = {
     reporter.reset()
--- scalafix-reflect/src/main/scala-3/scalafix/internal/reflect/RuleCompiler.scala
+++ scalafix-reflect/src/main/scala-3/scalafix/internal/reflect/RuleCompiler.scala
@@ -6,68 +6,68 @@ import dotty.tools.dotc.core.Contexts.FreshContext
 import dotty.tools.dotc.util.SourceFile
 import dotty.tools.dotc.reporting.StoreReporter
 import dotty.tools.dotc.interactive.InteractiveDriver
-import dotty.tools.io.AbstractFile as DottyAbstractFile
+import dotty.tools.io.AbstractFile
+import dotty.tools.io.Directory
+import dotty.tools.io.PlainDirectory
 import dotty.tools.io.VirtualFile
-import dotty.tools.io.VirtualDirectory as DottyVirtualDirectory
+import dotty.tools.io.VirtualDirectory
 import dotty.tools.repl.AbstractFileClassLoader
-
-import scala.reflect.io.VirtualDirectory
-import scala.reflect.io.AbstractFile
 
 import metaconfig.Configured
 import metaconfig.Input
 import metaconfig.ConfError
 import metaconfig.Position
 
+import java.io.File
 class RuleCompiler(
     classpath: String,
-    target: AbstractFile = new VirtualDirectory("(memory)", None)
+    targetDirectory: Option[File] = None
 ) {
-  private val settings = "-unchecked" :: "-deprecation" :: "-classpath" :: classpath :: Nil
+  private val output = targetDirectory match {
+    case Some(file) => new PlainDirectory(new Directory(file.toPath))
+    case None => new VirtualDirectory("(memory)")
+  }
+  private val settings =
+    "-unchecked" :: "-deprecation" :: "-classpath" :: classpath :: Nil
   private val driver = new InteractiveDriver(settings)
   private val reporter: StoreReporter = new StoreReporter()
   private var ctx: FreshContext = driver.currentCtx.fresh
-  private val dottyTargetDirectory: DottyAbstractFile = {
-    if (target.isVirtual)
-      new DottyVirtualDirectory(target.name)
-    else
-      DottyAbstractFile.getDirectory(target.path)
-  }
   ctx = ctx
     .setReporter(reporter)
-    .setSetting(ctx.settings.outputDir, dottyTargetDirectory)
+    .setSetting(ctx.settings.outputDir, output)
     .setSetting(ctx.settings.classpath, classpath)
 
   private val compiler: Compiler = new Compiler()
   private val classLoader: AbstractFileClassLoader =
-    new AbstractFileClassLoader(dottyTargetDirectory, this.getClass.getClassLoader)
-  
+    new AbstractFileClassLoader(output, this.getClass.getClassLoader)
+
   def compile(input: Input): Configured[ClassLoader] = {
     reporter.removeBufferedMessages(using ctx)
     val run: Run = compiler.newRun(using ctx)
 
-    val dottyFile: DottyAbstractFile = input match {
-      case Input.File(path, _) => DottyAbstractFile.getFile(input.path)
-      case Input.VirtualFile(path, _) => VirtualFile(input.path, input.text.getBytes())
+    val file: AbstractFile = input match {
+      case Input.File(path, _) => AbstractFile.getFile(input.path)
+      case Input.VirtualFile(path, _) =>
+        VirtualFile(input.path, input.text.getBytes())
       case _ => throw RuntimeException("Invalid Input file")
     }
 
     run.compileSources(
-      List(new SourceFile(dottyFile, input.chars))
+      List(new SourceFile(file, input.chars))
     )
 
-    val errors = reporter.allErrors.map(error => 
+    val errors = reporter.allErrors.map(error =>
       ConfError
         .message(error.getMessage)
     )
-    if (!errors.isEmpty) 
+    if (!errors.isEmpty)
       errors :+ ConfError.message(
-            "Error compiling rule(s) from source using Scala 3 compiler; " +
-            "to use the Scala 2.x compiler instead, use the corresponding " +
-            "scalafix-cli artifact or force scalafixScalaBinaryVersion " +
-            "to 2.x in your build tool"
-            )
-    
+        "Error compiling rule(s) from source using Scala 3 compiler; " +
+          "to use the Scala 2.x compiler instead, use the corresponding " +
+          "scalafix-cli artifact or force scalafixScalaBinaryVersion " +
+          "to 2.x in your build tool"
+      )
+
     ConfError
       .apply(errors)
       .map(_.notOk)
