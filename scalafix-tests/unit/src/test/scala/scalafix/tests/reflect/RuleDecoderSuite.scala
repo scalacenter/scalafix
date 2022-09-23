858003d0e70c284623f8fae84409fa8f2f2e0105
Brice Jaglin <bjaglin@gmail.com>
2022-09-23 02:14:27 +0200

    RuleDecoder: avoid error on Scala 3 & stale class on Scala 2
    In scala 3's AbstractFileClassLoader, `loadClass` is routing loadClass
    calls to findClass, causing "attempted duplicate class definition"
    when loading a rule sharing the same FQN as a previously compiled one.
    
    In scala 2, there is no failure but loaded rules could be the result
    of previous compilations, so returning a fresh classloader each time
    is probably what we want.

--- scalafix-tests/unit/src/test/scala/scalafix/tests/reflect/RuleDecoderSuite.scala
+++ scalafix-tests/unit/src/test/scala/scalafix/tests/reflect/RuleDecoderSuite.scala
@@ -10,6 +10,7 @@ import scalafix.internal.tests.utils.SkipWindows
 import scalafix.internal.v1.Rules
 import scalafix.tests.BuildInfo
 import scalafix.v1.RuleDecoder
+import java.nio.file.Files
 
 class RuleDecoderSuite extends AnyFunSuite {
   val cwd: AbsolutePath = AbsolutePath(BuildInfo.baseDirectory)
@@ -26,13 +27,42 @@ class RuleDecoderSuite extends AnyFunSuite {
     RuleDecoder.Settings().withCwd(cwd)
   val decoder: ConfDecoder[Rules] = RuleDecoder.decoder(decoderSettings)
   val expectedName = "NoDummy"
-  val expectedDescription = ""
+
   test("absolute path resolves as is", SkipWindows) {
     val rules = decoder.read(Conf.Str(s"file:$abspath")).get
     assert(expectedName == rules.name.value)
   }
+
   test("relative resolves from custom working directory") {
     val rules = decoder.read(Conf.Str(s"file:$relpath")).get
     assert(expectedName == rules.name.value)
+  }
+
+  test("resolved classes can be reloaded", SkipWindows) {
+    val tmp = Files.createTempFile("scalafix", "CustomRule.scala")
+
+    val customRuleV1 =
+      """package custom
+        |import scalafix.v1._
+        |class CustomRule extends SyntacticRule("CustomRule") {}
+      """.stripMargin
+    Files.write(tmp, customRuleV1.getBytes)
+    val rules1 =
+      decoder.read(Conf.Str(s"file:${tmp.toFile.getAbsolutePath}")).get
+    val class1 = rules1.rules.head.getClass
+
+    val customRuleV2 =
+      """package custom
+        |import scalafix.v1._
+        |class CustomRule extends SyntacticRule("CustomRule") {
+        |  def foo = 1
+        |}
+      """.stripMargin
+    Files.write(tmp, customRuleV2.getBytes)
+    val rules2 =
+      decoder.read(Conf.Str(s"file:${tmp.toFile.getAbsolutePath}")).get
+    val class2 = rules2.rules.head.getClass
+
+    assert(!class1.isAssignableFrom(class2))
   }
 }
