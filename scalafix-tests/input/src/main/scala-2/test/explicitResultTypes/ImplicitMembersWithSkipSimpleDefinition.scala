91ac7dc9b0ddc26db6038e6268a9edeb8b63a9d9
“Razvan <razvan.vacaru@tmnl.nl>
2022-10-28 14:08:54 +0200

    Added unit tests to reproduce bug on skipSimpleDefinitions

---
+++ scalafix-tests/input/src/main/scala-2/test/explicitResultTypes/ImplicitMembersWithSkipSimpleDefinition.scala
@@ -0,0 +1,13 @@
+/*
+rules = "ExplicitResultTypes"
+ExplicitResultTypes.skipSimpleDefinitions = ["Lit"]
+*/
+package test.explicitResultTypes
+
+object ImplicitMembersWithSkipSimpleDefinition {
+
+    val x = 42
+
+    implicit var y = 43
+  
+}
---
+++ scalafix-tests/output/src/main/scala-2/test/explicitResultTypes/ImplicitMembersWithSkipSimpleDefinition.scala
@@ -0,0 +1,9 @@
+package test.explicitResultTypes
+
+object ImplicitMembersWithSkipSimpleDefinition {
+
+    val x: Int = 42
+
+    implicit var y: Int = 43
+  
+}
