10481f245d791204d29be239e5767268e2782e03
Brice Jaglin <bjaglin@gmail.com>
2023-06-03 15:16:03 +0200

    post 0.11.0

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -7,10 +7,10 @@ cs resolve \
   ch.epfl.scala:scalafix-interfaces:$version  \
   ch.epfl.scala:scalafix-core_2.12:$version  \
   ch.epfl.scala:scalafix-core_2.13:$version  \
-  ch.epfl.scala:scalafix-reflect_2.12.17:$version  \
-  ch.epfl.scala:scalafix-reflect_2.13.10:$version  \
-  ch.epfl.scala:scalafix-cli_2.12.17:$version  \
-  ch.epfl.scala:scalafix-cli_2.13.10:$version  \
-  ch.epfl.scala:scalafix-testkit_2.12.17:$version \
-  ch.epfl.scala:scalafix-testkit_2.13.10:$version \
+  ch.epfl.scala:scalafix-reflect_2.12.18:$version  \
+  ch.epfl.scala:scalafix-reflect_2.13.11:$version  \
+  ch.epfl.scala:scalafix-cli_2.12.18:$version  \
+  ch.epfl.scala:scalafix-cli_2.13.11:$version  \
+  ch.epfl.scala:scalafix-testkit_2.12.18:$version \
+  ch.epfl.scala:scalafix-testkit_2.13.11:$version \
   -r sonatype:public
