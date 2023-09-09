fd35cf2b6e63fb251e73a152a0badede1b93ab75
Brice Jaglin <bjaglin@gmail.com>
2023-09-09 21:14:50 +0200

    add test

---
+++ scalafix-tests/input/src/main/scala-2/test/Headers.scala
@@ -0,0 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/*
+ * Copyright (C) xxxxxxx
+ */
+
+/*
+rules = [
+  "class:scalafix.test.NoDummy"
+]
+*/
+package test
+
+class Headers {
+  val aDummy = 0 // assert: NoDummy
+}
+
