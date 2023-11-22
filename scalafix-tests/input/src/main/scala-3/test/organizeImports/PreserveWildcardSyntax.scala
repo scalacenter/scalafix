36a9792a668786d18fd7d07c509376c989bd9ca6
Brice Jaglin <bjaglin@gmail.com>
2023-11-22 15:02:34 +0100

    OrganizeImports: document which wildcard syntax is used

---
+++ scalafix-tests/input/src/main/scala-3/test/organizeImports/PreserveWildcardSyntax.scala
@@ -0,0 +1,11 @@
+/*
+rules = [OrganizeImports]
+OrganizeImports.removeUnused = false
+ */
+package test.organizeImports
+
+import test.organizeImports.GivenImports._
+
+import scala.util.*
+
+object PreserveWildcardSyntax
---
+++ scalafix-tests/output/src/main/scala-3/test/organizeImports/PreserveWildcardSyntax.scala
@@ -0,0 +1,7 @@
+package test.organizeImports
+
+import test.organizeImports.GivenImports._
+
+import scala.util.*
+
+object PreserveWildcardSyntax
