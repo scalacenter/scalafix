2b0d32ddfeb1cde2d1e08f25cd8d374c5c9b9da4
Brice Jaglin <bjaglin@gmail.com>
2024-01-11 20:13:55 +0100

    Origin is now part of the public API

--- scalafix-core/src/main/scala/scala/meta/internal/scalafix/ScalafixScalametaHacks.scala
+++ scalafix-core/src/main/scala/scala/meta/internal/scalafix/ScalafixScalametaHacks.scala
@@ -1,10 +1,7 @@
 package scala.meta.internal.scalafix
 
-import scala.meta.Tree
 import scala.meta.internal.semanticdb.Scala.Names
-import scala.meta.internal.trees.Origin
 
 object ScalafixScalametaHacks {
-  def resetOrigin(tree: Tree): Tree = tree.withOrigin(Origin.None)
   def encode(name: String): String = Names.encode(name)
 }
