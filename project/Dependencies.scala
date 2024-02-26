bd1de51c28794010c1aa55a2c6bdfe2a4fa8f9e7
Brice Jaglin <bjaglin@gmail.com>
2024-02-26 09:14:26 +0100

    Latest scalameta

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val metaconfigV = "0.12.0"
   val nailgunV = "0.9.1"
   val scalaXmlV = "2.2.0"
-  val scalametaV = "4.9.0"
+  val scalametaV = "4.9.0+35-d8feb069-SNAPSHOT"
   val scalatestV = "3.2.18"
   val munitV = "0.7.29"
 
