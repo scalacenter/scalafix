909248c3e7fa3bb9a66e32dcbcaee725bacf81d1
Brice Jaglin <bjaglin@gmail.com>
2024-05-01 17:21:43 +0200

    sbt-scalafix scalafixScalaBinaryVersion is now deprecated

--- project/ScalafixBuild.scala
+++ project/ScalafixBuild.scala
@@ -56,11 +56,10 @@ object ScalafixBuild extends AutoPlugin with GhpagesKeys {
           }
 
           val prevVersions = previousVersions(sv).map(prev => TargetAxis(prev))
-          val scala3FromScala2 = TargetAxis(scala3)
           val xsource3 = TargetAxis(sv, xsource3 = true)
 
-          (prevVersions :+ scala3FromScala2 :+ xsource3).map((sv, _))
-        }
+          (prevVersions :+ xsource3).map((sv, _))
+        } :+ (scala213, TargetAxis(scala3))
 
     lazy val publishLocalTransitive =
       taskKey[Unit]("Run publishLocal on this project and its dependencies")
