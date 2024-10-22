e3ad9c3ba296b12329678f6f0ea8ab8ce995d349
Brice Jaglin <bjaglin@gmail.com>
2024-10-23 01:07:29 +0200

    Update scalameta to 4.10.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val metaconfigV = "0.13.0"
   val nailgunV = "0.9.1"
   val scalaXmlV = "2.2.0"
-  val scalametaV = "4.10.1"
+  val scalametaV = "4.10.2"
   val scalatestV = "3.2.19"
   val munitV = "1.0.2"
 
