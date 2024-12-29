ebdee660880a2a4a0f63298314ea1fb6855b1842
Brice Jaglin <bjaglin@gmail.com>
2024-12-29 21:43:35 +0100

    parse Scala 3 nightly versions

--- scalafix-tests/unit/src/test/scala/scalafix/tests/config/ScalaVersionSuite.scala
+++ scalafix-tests/unit/src/test/scala/scalafix/tests/config/ScalaVersionSuite.scala
@@ -22,12 +22,28 @@ class ScalaVersionSuite extends munit.FunSuite {
 
   test("parse: 2.13.16-bin-ce78754") {
     val scala2 = ScalaVersion.from("2.13.16-bin-ce78754")
-    assert(scala2.get == Nightly(Major.Scala2, 13, 16, "ce78754"))
+    assert(scala2.get == Nightly(Major.Scala2, 13, 16, None, None, "ce78754"))
+    assert(scala2.get.value == "2.13.16-bin-ce78754")
   }
 
   test("parse: 3.0.0-RC3") {
     val scala3 = ScalaVersion.from("3.0.0-RC3")
     assert(scala3.get == RC(Major.Scala3, 0, 0, 3))
+  }
+
+  test("parse: 3.6.0-RC1-bin-20241008-3408ed7-NIGHTLY") {
+    val scala3 = ScalaVersion.from("3.6.0-RC1-bin-20241008-3408ed7-NIGHTLY")
+    assert(
+      scala3.get == Nightly(
+        Major.Scala3,
+        6,
+        0,
+        Some(1),
+        Some(java.time.LocalDate.parse("2024-10-08")),
+        "3408ed7"
+      )
+    )
+    assert(scala3.get.value == "3.6.0-RC1-bin-20241008-3408ed7-NIGHTLY")
   }
 
   test("parse failure: 3.0.0RC3") {
