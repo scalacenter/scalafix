44e8d87e307f2a93d225fcf6ee5005ba797bb07c
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2026-04-23 08:46:25 -0700

    Patching: use Tokens own traversal methods
    Avoids deprecated methods as well.

--- scalafix-core/src/main/scala/scalafix/util/TokenClasses.scala
+++ scalafix-core/src/main/scala/scalafix/util/TokenClasses.scala
@@ -3,12 +3,10 @@ package scalafix.util
 import scala.meta.classifiers.Classifier
 import scala.meta.classifiers.XtensionClassifiable
 import scala.meta.tokens.Token
-import scala.meta.tokens.Token._
 
 trait Whitespace
 object Whitespace {
-  def unapply(token: Token): Boolean =
-    token.is[Space] || token.is[Tab] || token.is[Newline] || token.is[FF]
+  def unapply(token: Token): Boolean = token.is[Token.Whitespace]
   implicit def classifier[T <: Token]: Classifier[T, Whitespace] =
     new Classifier[T, Whitespace] {
       override def apply(token: T): Boolean = unapply(token)
@@ -17,8 +15,7 @@ object Whitespace {
 
 trait Trivia
 object Trivia {
-  def unapply(token: Token): Boolean =
-    token.is[Whitespace] || token.is[Comment]
+  def unapply(token: Token): Boolean = token.is[Token.Trivia]
   implicit def classifier[T <: Token]: Classifier[T, Trivia] =
     new Classifier[T, Trivia] {
       override def apply(token: T): Boolean = unapply(token)
@@ -27,8 +24,7 @@ object Trivia {
 
 trait Newline
 object Newline {
-  def unapply(token: Token): Boolean =
-    token.is[LF] || token.is[CR]
+  def unapply(token: Token): Boolean = token.is[Token.AtEOL]
   implicit def classifier[T <: Token]: Classifier[T, Newline] =
     new Classifier[T, Newline] {
       override def apply(token: T): Boolean = unapply(token)
