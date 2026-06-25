54d57019a5548e06f26fcc1d0e657bf7fef002b2
meriam lachkar <meriam.lachkar@gmail.com>
2026-06-25 09:09:46 +0200

    OrganizeImports: preserve trailing comma in multi-line imports
    Since the switch to scalameta pretty-reprinting (0d232c9f), the rule
      stopped re-emitting the trailing comma when reprinting a multi-line
      import, even when nothing else changed. On an already-sorted import the
      rule would still produce a diff, just to strip the comma.

--- scalafix-tests/output/src/main/scala-3/test/organizeImports/MultiLineImportsWithAliasesReorder.scala
+++ scalafix-tests/output/src/main/scala-3/test/organizeImports/MultiLineImportsWithAliasesReorder.scala
@@ -1,10 +1,12 @@
 package test.organizeImports
 
-// Imports are in WRONG order: TreeMap before HashMap - rule must reorder
+// Imports are in WRONG order, mixing aliased and plain names - rule must reorder
 import scala.collection.immutable.{
   ArraySeq as ASeq,
   HashMap as HMap,
-  TreeMap as TMap
+  Queue,
+  TreeMap as TMap,
+  Vector,
 }
 
 object MultiLineImportsWithAliasesReorder
