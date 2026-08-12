18830bc0389f4f690ee6313f02c48c4a2bd331d6
Jozef Koval <jozef.koval@protonmail.ch>
2026-08-12 22:04:02 +0200

    Remove no-op global caches in ScalafixToolbox (fix #782)

--- scalafix-reflect/src/main/scala/scalafix/internal/reflect/ScalafixToolbox.scala
+++ scalafix-reflect/src/main/scala/scalafix/internal/reflect/ScalafixToolbox.scala
@@ -1,26 +1,12 @@
 package scalafix.internal.reflect
 
 import java.net.URLClassLoader
-import java.util.function
 
 import metaconfig.Configured
 import metaconfig.Input
 import scalafix.internal.config.MetaconfigOps._
 
-object ScalafixToolbox extends ScalafixToolbox
-class ScalafixToolbox {
-
-  private val ruleCache =
-    new java.util.concurrent.ConcurrentHashMap[Input, Configured[
-      CompiledRules
-    ]]()
-  private val compilerCache =
-    new java.util.concurrent.ConcurrentHashMap[URLClassLoader, RuleCompiler]()
-  private val newCompiler: function.Function[URLClassLoader, RuleCompiler] =
-    new function.Function[URLClassLoader, RuleCompiler] {
-      override def apply(toolClasspath: URLClassLoader) =
-        new RuleCompiler(toolClasspath)
-    }
+object ScalafixToolbox {
 
   case class CompiledRules(classloader: ClassLoader, fqns: Seq[String])
 
@@ -28,28 +14,8 @@ class ScalafixToolbox {
       code: Input,
       toolClasspath: URLClassLoader
   ): Configured[CompiledRules] =
-    ruleCache.getOrDefault(
-      code, {
-        val uncached = getRuleUncached(code, toolClasspath)
-        uncached match {
-          case toCache @ Configured.Ok(_) =>
-            ruleCache.put(code, toCache)
-          case _ =>
-        }
-        uncached
-      }
-    )
-
-  def getRuleUncached(
-      code: Input,
-      toolClasspath: URLClassLoader
-  ): Configured[CompiledRules] =
-    synchronized {
-      val compiler =
-        compilerCache.computeIfAbsent(toolClasspath, newCompiler)
-      (
-        compiler.compile(code) |@|
-          RuleInstrumentation.getRuleFqn(code.toMeta)
-      ).map((CompiledRules.apply _).tupled)
-    }
+    (
+      new RuleCompiler(toolClasspath).compile(code) |@|
+        RuleInstrumentation.getRuleFqn(code.toMeta)
+    ).map((CompiledRules.apply _).tupled)
 }
